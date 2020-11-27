022000af241e5c375552b710e47937e64de717bc
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-11-27 21:11:21 +0100

    Add menu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+	void Awake()
+	{
+		//TODO initialize gamemanager
+	}
+
+	public void HandleOnStateChange()
+	{
+		Debug.Log("OnStateChange mainmenu!");
+	}
+
+	public void LevelSelect()
+	{
+		//TODO is for level select stuff
+	}
+
+	public void StartGame()
+	{
+		//TODO go to first or next level
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quit!");
+		Application.Quit();
+	}
+}

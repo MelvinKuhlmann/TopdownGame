f2bdc5805a93e7ed0d8d4f9030bbced27617ec22
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-12-05 16:24:19 +0100

    Add screen fader for switching scenes. Need to fix a bug on the host though...

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -9,11 +9,38 @@ public class AreaExit : MonoBehaviour
 
     public string areaTransitionName = string.Empty;
 
+    public float waitToLoad = 1f;
+    private bool shouldLoadAfterFade;
+
+    private void Start()
+    {
+        if (UIFade.instance != null)
+        {
+            UIFade.instance.FadeFromBlack();
+        }
+    }
+
+    private void Update()
+    {
+        if (shouldLoadAfterFade)
+        {
+            waitToLoad -= Time.deltaTime;
+            if (waitToLoad <= 0)
+            {
+                shouldLoadAfterFade = false;
+                FindObjectOfType<TopDownNetworkManager>().ServerChangeScene(sceneToLoad);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == Tag.Player.ToString())
         {
-            FindObjectOfType<TopDownNetworkManager>().ServerChangeScene(sceneToLoad);
+            // FindObjectOfType<TopDownNetworkManager>().ServerChangeScene(sceneToLoad);
+
+            shouldLoadAfterFade = true;
+            UIFade.instance.FadeToBlack();
         }
     }
 }

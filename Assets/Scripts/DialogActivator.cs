2723914928337118cbe1ee23723d12304c09f2c4
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-12-07 20:40:15 +0100

    Added dialog system

---
+++ Assets/Scripts/DialogActivator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class DialogActivator : MonoBehaviour
+{
+
+    public string[] lines;
+    public bool isPerson = true;
+    private bool canActivate;
+
+
+    void Update()
+    {
+        if (canActivate && Input.GetKeyDown(KeyCode.Return) && !DialogManager.instance.dialogBox.activeInHierarchy)
+        {
+            DialogManager.instance.ShowDialog(lines, isPerson);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == Tag.Player.ToString())
+        {
+            canActivate = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == Tag.Player.ToString())
+        {
+            canActivate = false;
+            if (DialogManager.instance.dialogBox.activeInHierarchy)
+            {
+                DialogManager.instance.dialogBox.SetActive(false);
+            }
+        }
+    }
+}

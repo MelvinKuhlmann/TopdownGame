bcd91e16b6c5fbc8db7d49f8e7ccd482a98372e6
Melvin Kuhlmann <melvin.kuhlmann@gmail.com>
2020-12-10 00:26:22 +0100

    Started foundation of pickupable items

---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    public BoxCollider2D boxCollider2D;
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+    // This will be the value of the t.b.d. currency we will use in the game.
+    private int value { get; set; }
+}

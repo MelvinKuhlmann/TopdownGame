f635669cccc47f991335eee51169b07be42cec07
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-12-31 16:50:53 +0100

    #3 fill panels with items

--- Assets/Scripts/Shop/ShopUI.cs
+++ Assets/Scripts/Shop/ShopUI.cs
@@ -6,10 +6,16 @@ public class ShopUI : MonoBehaviour
 {
     public GameObject shopMenu;
     public GameObject buyMenu;
+    public GameObject buyMenuItems;
     public GameObject sellMenu;
+    public GameObject sellMenuItems;
     public TMP_Text shardText;
+    public GameObject shopItem;
 
     private List<Item> itemsInShop;
+
+    private ItemButton[] buyItemButtons;
+    private ItemButton[] sellItemButtons;
 
     #region Singleton
     public static ShopUI instance;
@@ -44,11 +50,41 @@ public class ShopUI : MonoBehaviour
     {
         buyMenu.SetActive(true);
         sellMenu.SetActive(false);
+        ClearItems(buyMenuItems.transform);
+
+        for (int i = 0; i < itemsInShop.Count; i++)
+        {
+            GameObject item = Instantiate(shopItem, new Vector3(0, 0, 0), Quaternion.identity);
+            item.transform.SetParent(buyMenuItems.transform, false);
+
+            ItemButton itemButton = item.GetComponent<ItemButton>();
+            itemButton.itemImage.sprite = itemsInShop[i].icon;
+            itemButton.amountText.text = "";
+        }
     }
 
     public void OpenSellMenu()
     {
         buyMenu.SetActive(false);
         sellMenu.SetActive(true);
+        ClearItems(sellMenuItems.transform);
+
+        for (int i = 0; i < itemsInShop.Count; i++) // TODO get from inventory instead of NPC
+        {
+            GameObject item = Instantiate(shopItem, new Vector3(0, 0, 0), Quaternion.identity);
+            item.transform.SetParent(sellMenuItems.transform, false);
+
+            ItemButton itemButton = item.GetComponent<ItemButton>();
+            itemButton.itemImage.sprite = itemsInShop[i].icon; // TODO get from inventory instead of NPC
+            itemButton.amountText.text = "13";
+        }
+    }
+
+    private void ClearItems(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
     }
 }

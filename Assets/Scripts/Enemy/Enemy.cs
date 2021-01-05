b897f7d27d32855fc84e6e1a1fb89df0f90efef6
Melvin Kuhlmann <melvin.kuhlmann@gmail.com>
2021-01-06 00:14:22 +0100

    Added Enemy.cs; removed IEnemy.cs for easier abstraction.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public abstract int id { get; }
+
+    public abstract int maxHealth { get; }
+
+    public abstract int experience { get; }
+    public abstract int level { get; }
+
+    public abstract int moveSpeed { get; }
+
+    public new abstract string name { get; }
+
+    public int currentHealth { get; set; }
+
+    protected void Die()
+    {
+        CombatEvents.EnemyDied(this);
+    }
+
+    protected void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    /// <summary>
+    /// Implement this method for adding attack logic
+    /// </summary>
+    protected abstract void PerformAttack();
+}
